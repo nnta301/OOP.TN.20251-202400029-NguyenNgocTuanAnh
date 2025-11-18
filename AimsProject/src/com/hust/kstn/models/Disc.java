7c4f7d80fb0b042c2862a991d0450cc3c83450ce
nnta301 <tuananh3012006@gmail.com>
2025-11-18 23:50:59 +0700

    refactor: change CompactDisc and DigitalVideoDisc to extend Disc class

---
+++ AimsProject/src/com/hust/kstn/models/Disc.java
@@ -0,0 +1,7 @@
+package com.hust.kstn.models;
+
+public abstract class Disc extends Media {
+    public Disc(String title, double cost, String category) {
+        super(title, cost, category);
+    }
+}
